0658f4a7abcdedfb382edbaff9dfca3634b03f54
Srikar <srikarboina9999@gmail.com>
2024-12-12 01:03:06 +0530

    3rd commit

--- prime.java
+++ prime.java
@@ -1,13 +1,18 @@
 public class prime {
     public static void main(String[] args) {
+        // Loop through numbers from 2 to 100
         for (int num = 2; num <= 100; num++) {
             boolean isPrime = true;
-            for (int i = 2; i <= Math.sqrt(num); i++) {  // Optimized to check up to the square root of num
+            
+            // Check divisibility from 2 to the square root of num
+            for (int i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0) {
-                    isPrime = false;
+                    isPrime = false;  // num is not prime if divisible by i
                     break;
                 }
             }
+            
+            // If isPrime is still true, the number is prime
             if (isPrime) {
                 System.out.println(num);
             }
