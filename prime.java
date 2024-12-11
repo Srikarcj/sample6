33b7a1e6d7a10e28f3d07321bff68deb46c62130
Srikar <srikarboina9999@gmail.com>
2024-12-12 01:04:31 +0530

    5th commit

--- prime.java
+++ prime.java
@@ -1,21 +1,27 @@
 public class prime {
     public static void main(String[] args) {
-        // Loop through numbers starting from 2 (since 1 is not prime)
+        // StringBuilder to store prime numbers
+        StringBuilder primeNumbers = new StringBuilder("Prime numbers from 1 to 100:\n");
+        
+        // Loop through numbers starting from 2
         for (int num = 2; num <= 100; num++) {
             boolean isPrime = true;
             
             // Check divisibility from 2 to the square root of num
             for (int i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0) {
-                    isPrime = false;  // num is not prime if divisible by i
+                    isPrime = false;
                     break;
                 }
             }
             
-            // If isPrime is still true, the number is prime
+            // If num is prime, add to the result string
             if (isPrime) {
-                System.out.println(num);
+                primeNumbers.append(num).append(" ");
             }
         }
+        
+        // Print all prime numbers in a formatted list
+        System.out.println(primeNumbers.toString());
     }
 }
